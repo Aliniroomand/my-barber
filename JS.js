fbd8ee0c12f346dd4a95774e85490d278582e8da
Aliniroomand <aliniroomand32@gmail.com>
2023-06-27 11:06:32 +0430

    fixing the problem of choosing gender

--- JS.js
+++ JS.js
@@ -4,23 +4,22 @@
 
  // 1-for first page and select gender:
 
- const selectManGender=document.querySelector("#men-gender");
- const selectWomanGender=document.querySelector("#women-gender");
-
+ const selectManGender=document.querySelector(".choosing-at-first-men");
+ const selectWomanGender=document.querySelector(".choosing-at-first-women");
+ 
  selectManGender.addEventListener("click",openSelectedGender);
  selectWomanGender.addEventListener("click",openSelectedGender);
-
  function openSelectedGender(event){
-    event 
-    const ID=event.target.id;
-     if(ID=== "men-gender"){
-        event.target.classList=("dissapear-choose-gender")  
-        
-        console.log(image);
+    const classOfSelected=event.target.className;
+    console.log(event.target.className)
+     if( classOfSelected === "choosing-at-first-men"){
+        selectWomanGender.className=("dissapear-notchoosen-gender")  
+        console.log(selectManGender.className)
+
         }
-        if(ID=== "women-gender"){
+        if(classOfSelected === "choosing-at-first-women"){
             
-        event.target.classList="dissapear-choose-gender" 
+         selectManGender.className=("dissapear-notchoosen-gender")  
 
    
            }
@@ -28,4 +27,4 @@
 //2-for loading
 const loadingMen=document.querySelector(".loading-title-men");
 loadingMen.style.display="none";
-console.log(loadingMen);
+
