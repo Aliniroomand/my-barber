576ea11a303b713447d1865e7e1222e745734fb8
Aliniroomand <aliniroomand32@gmail.com>
2023-07-01 12:24:31 +0430

    fix the problems of loading animations that was:they didn't come center

--- JS.js
+++ JS.js
@@ -11,21 +11,25 @@
  selectWomanGender.addEventListener("click",openSelectedGender);
  function openSelectedGender(event){
     const classOfSelected=event.target.className;
-    console.log(event.target.className)
+
      if( classOfSelected === "choosing-at-first-men"){
         selectWomanGender.className=("dissapear-notchoosen-gender")  
-        loadingMen.style.display="block"
+        loadingMen.style.display="flex";
+        loadingMen.style.animation=" goAndDisappear 3s linear forwards";
+
 
         }
         if(classOfSelected === "choosing-at-first-women"){
-            
-         selectManGender.className=("dissapear-notchoosen-gender")  
-         loadingWomen.style.display="block";
+         selectManGender.className=("dissapear-notchoosen-gender") ;
+        loadingMen.innerHTML=
+        `<figure class="loading-men" style= "animation:goAndDisappear 3s linear forwards;display: flex;" >
+            <img src="./images/loading animated/women-loading.gif" class="animateForLoading-men"  alt="men-loading">
+            <h2 class="loading-title" style="text-shadow: 2px 2px 5px red,-2px -2px 5px red;z-index: 10;color: rgba(0, 0, 0, 0.795);">loading</h2> 
+         </figure>`;
+        loadingMen.style.display="flex";
 
-   
            }
-}
-//2-for loading
-const loadingWomen=document.querySelector(".loading-women");
+         }      
+           //2-for loading
 const loadingMen=document.querySelector(".loading-men");
 
